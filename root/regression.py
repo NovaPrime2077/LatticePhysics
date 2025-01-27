611e7e289fa7095cb0e5e3fccd37b42e29e2c255
NovaPrime2077 <hypernova2189@gmail.com>
2025-01-27 22:38:12 +0530

    regression.py

--- root/regression.py
+++ root/regression.py
@@ -1,14 +1,11 @@
 import numpy as np
 import copy,math
 
-
-
 def cost_function(x,y,w,b):
     m = x.shape[0]
     f_wb = np.dot(x, w) + b  # Predictions for all samples
     cost = np.sum((f_wb - y) ** 2) / (2 * m)  # Vectorized cost computation
     return cost
-
 
 def gradient(x,y,w,b):
     m = x.shape[0]
@@ -17,8 +14,6 @@ def gradient(x,y,w,b):
     dj_dw = np.dot(x.T, error) / m 
     dj_db = np.sum(error) / m 
     return dj_dw, dj_db
-
-
 
 def gradient_descent(x,y,w_in,b_in,alpha,num_iteration,cost_function,gradient):
     J_hist = []
@@ -34,12 +29,8 @@ def gradient_descent(x,y,w_in,b_in,alpha,num_iteration,cost_function,gradient):
             J_hist.append(cost_function(x,y,w,b))
     return w,b,J_hist
 
-
 def regression_func(x, y,iterations, alpha):
     w = np.random.randn(len(x[0]))
     b_in = 0
     w_final,b_final,hist = gradient_descent(x,y,w,b_in,alpha,iterations,cost_function,gradient)
     return w_final, b_final,hist
-
-
-    
